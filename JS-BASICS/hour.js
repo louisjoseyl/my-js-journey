161a34c945a2f844b0c330e929575f1de4194b5a
Louis Joseph <louisjosephleadway@gmail.com>
2023-09-25 00:30:49 +0100

    calculate the average grade from an array

--- JS-BASICS/hour.js
+++ JS-BASICS/hour.js
@@ -2,11 +2,11 @@
 // // if the hour is between 6am and 12pm : good morning 
 // // if hour is between 12 pm and 6pm : good afternoon
 // //otherwise: good evening
-greet (18)
+greet (13)
 function greet(hour){
   if(hour < 12)
   console.log('Good morning')
   else if(hour < 18)
   console.log('good afternoon')
-  else console.log ('good evening')
+  else console.log ('good evening!')
 }
